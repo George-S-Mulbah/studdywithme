c61788ab773adb7b70b06241c2d492e9c912d294
sirgeorge <gsmulbah2500@gmail.com>
2023-02-10 06:22:56 +0000

    Started work on the form

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('room/<str:pk>/',views.room,name="room")
+    path('room/<str:pk>/',views.room,name="room"),
+    path('createRoom/',views.createRoom,name="createRoom")
 ]
